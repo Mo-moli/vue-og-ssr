1bb7ceb2410eecc8b2a6ad91dedfaea449d58858
lili <sabai930808@gmail.com>
2025-07-07 16:16:55 +0800

    fix: vite-plugin-ssr build config for Vercel

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,10 @@
-// vite.config.ts
-import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import ssr from 'vite-plugin-ssr/plugin'
+import { defineConfig } from 'vite'
 
 export default defineConfig({
   plugins: [vue(), ssr()],
+  ssr: {
+    noExternal: ['vite-plugin-ssr'] // 確保 SSR 正常
+  }
 })
