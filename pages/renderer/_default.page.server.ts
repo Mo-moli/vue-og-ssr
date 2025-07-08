3df07a9f06d82b7a8f02b995ce7b2263b3d8a416
lili <sabai930808@gmail.com>
2025-07-08 10:57:42 +0800

    feat:"避免 XSS 風險"

--- pages/renderer/_default.page.server.ts
+++ pages/renderer/_default.page.server.ts
@@ -1,32 +1,34 @@
 export { render }
 export { passToClient }
+
 import { renderToString } from 'vue/server-renderer'
 import { createApp } from './app'
+import { escapeInject, dangerouslySkipEscape } from 'vite-plugin-ssr/server'
 
-const passToClient = ['documentProps']  
+const passToClient = ['documentProps']
 
 async function render(pageContext: any) {
   const app = createApp(pageContext)
   const appHtml = await renderToString(app)
 
   const title = pageContext.documentProps?.title || 'My Default Title'
+  const id = pageContext.routeParams?.id ?? ''
 
+  const documentHtml = escapeInject`<!DOCTYPE html>
+    <html lang="en">
+      <head>
+        <meta charset="UTF-8" />
+        <title>${title}</title>
+        <meta property="og:title" content="${title}" />
+        <meta property="og:type" content="website" />
+        <meta property="og:url" content="https://你的網址/service-us/${id}" />
+      </head>
+      <body>
+        <div id="app">${dangerouslySkipEscape(appHtml)}</div>
+      </body>
+    </html>`
 
   return {
-    documentHtml: `
-      <!DOCTYPE html>
-      <html lang="en">
-        <head>
-          <meta charset="UTF-8" />
-          <title>${title}</title>
-          <meta property="og:title" content="${title}" />
-          <meta property="og:type" content="website" />
-          <meta property="og:url" content="https://你的網址/service-us/${pageContext.routeParams.id}" />
-        </head>
-        <body>
-          <div id="app">${appHtml}</div>
-        </body>
-      </html>
-    `
+    documentHtml
   }
 }
